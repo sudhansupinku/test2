0f3d98e3e50abb6747391d45c959663291617683
sudhansu14 <sudhansu-sekhar.jenamani@capgemini.com>
2022-08-13 01:02:47 +0530

    first commit

---
+++ kanti.js
@@ -0,0 +1,97 @@
+var pageData = {
+	'sale_id': '17d36f89-40a4-4ec4-b8de-fc19f7f72c39',
+	'sale_signals': {
+		'DPXFU5YX73PJDV9M': {
+			'signal_namespaces': {},
+			'listings': {
+				'LSTDPXFU5YX73PJDV9MCXH6GS': {
+					'signal_namespaces': {
+						'SLM': {
+							'namespace': {
+								'name': 'SLM',
+								'details': 'Seller listing management'
+							},
+							'signals': [{
+								'namespace': {
+									'name': 'SLM',
+									'details': 'Seller listing management'
+								},
+								'name': 'Listing exclusivity',
+								'details': 'Marketplace exclusivity check',
+								'flag': 'RED',
+								'timeline': 'N-3',
+								'fix_type': 'HELP_MESSAGE',
+								'message': 'Product is not Flo-exclusive - PoC:<a href="mailto:sp-slm-dev-ops@flipkart.com" target="_top">slm-devops</a>',
+								'signal_marker': 'LISTING',
+								'oncall_alias': None,
+								'raw_error': None,
+								'fix_provider': 'ListingExclusivitySignalProvider'
+							}]
+						}
+					}
+				},
+				  'LSTDPXFU5YX73PJDV9MCXH6GS': {
+					'signal_namespaces': {
+						'SLM': {
+							'namespace': {
+								'name': 'SLM',
+								'details': 'Seller listing management'
+							},
+							'signals': [{
+								'namespace': {
+									'name': 'SLM',
+									'details': 'Seller listing management'
+								},
+								'name': 'Listing exclusivity',
+								'details': 'Marketplace exclusivity check',
+								'flag': 'RED',
+								'timeline': 'N-3',
+								'fix_type': 'HELP_MESSAGE',
+								'message': 'Product is not Flo-exclusive - PoC:<a href="mailto:sp-slm-dev-ops@flipkart.com" target="_top">slm-devops</a>',
+								'signal_marker': 'LISTING',
+								'oncall_alias': None,
+								'raw_error': None,
+								'fix_provider': 'ListingExclusivitySignalProvider'
+							}]
+						}
+					}
+				}
+				  
+			}
+		},
+		'ACNERYHAG3JCAR6H': {
+			'signal_namespaces': {},
+			'listings': {
+				'LSTACNERYHAG3JCAR6HZJ1WTF': {
+					'signal_namespaces': {
+						'SLM': {
+							'namespace': {
+								'name': 'SLM',
+								'details': 'Seller listing management'
+							},
+							'signals': [{
+								'namespace': {
+									'name': 'SLM',
+									'details': 'Seller listing management'
+								},
+								'name': 'Listing exclusivity',
+								'details': 'Marketplace exclusivity check',
+								'flag': 'RED',
+								'timeline': 'N-3',
+								'fix_type': 'HELP_MESSAGE',
+								'message': 'Product is not Flo-exclusive - PoC:<a href="mailto:sp-slm-dev-ops@flipkart.com" target="_top">slm-devops</a>',
+								'signal_marker': 'LISTING',
+								'oncall_alias': None,
+								'raw_error': None,
+								'fix_provider': 'ListingExclusivitySignalProvider'
+							}]
+						}
+					}
+				}
+			}
+		}
+	},
+	'sale_eta': '1996-01-01 15:30:00',
+	'sale_owner': 'ajith.km',
+	'created_on': '2022-04-05 14:41:59.342535'
+}
